28c721aa60e4684e0551822f8fea010636cd4c60
install <install@KI-3095.local>
2019-09-28 22:35:16 +0200

    Refactor code

--- backend/app.py
+++ backend/app.py
@@ -22,9 +22,8 @@ MODELS = {
     # "fr_core_news_sm": spacy.load("fr_core_news_sm"),
     # "it_core_news_sm": spacy.load("it_core_news_sm"),
     # "nl_core_news_sm": spacy.load("nl_core_news_sm"),
-    "latest": spacy.load(Path.home() / 'models' / 'latest')
+    "latest": spacy.load((Path.home() / 'models' / 'latest').mkdir(parents=True, exist_ok=True))
 }
-
 
 def get_model_desc(nlp, model_name):
     """Get human-readable model name, language name and version."""
@@ -33,33 +32,16 @@ def get_model_desc(nlp, model_name):
     model_version = nlp.meta["version"]
     return "{} - {} (v{})".format(lang_name, model_name, model_version)
 
-
 @app.route("/models", methods=['GET'])
 def models():
     return {name: get_model_desc(nlp, name) for name, nlp in MODELS.items()}
-
-
-# @app.route("/dep", methods=['POST'])
-# def dep(
-#     text: str,
-#     model: str,
-#     collapse_punctuation: bool = False,
-#     collapse_phrases: bool = False,
-# ):
-#     """Get dependencies for displaCy visualizer."""
-#     nlp = MODELS[model]
-#     doc = nlp(text)
-#     options = {
-#         "collapse_punct": collapse_punctuation,
-#         "collapse_phrases": collapse_phrases,
-#     }
-#     return spacy.displacy.parse_deps(doc, options)
 
 
 @app.route("/ent", methods=['POST'])
 def ent():
     """Get entities for displaCy ENT visualizer."""
     model = request.json["model"]
+
     text = request.json["text"]
     nlp = MODELS[model]
     doc = nlp(text)
@@ -77,6 +59,7 @@ def ent():
 def update():
     """Update entities based on annotation (and annotate document)."""
     model = request.json["model"]
+
     redactions = request.json["redactions"]
     text = redactions["text"]
     offsets = redactions["offsets"]
@@ -101,29 +84,6 @@ def testMethod():
         for ent in doc.ents
     ]}
 
-# def train_ner(nlp, training_data, iterations, entity_types):
-#     """Use the training data to update the model."""
-#     for label in entity_types:
-#         nlp.entity.add_label(label)
-
-#     for text, _ in training_data:
-#         for word in nlp.make_doc(text):
-#             nlp.vocab[word.orth]  # suppress(pointless-statement)
-
-#     for _ in range(0, iterations):
-#         random.shuffle(training_data)
-#         loss = 0
-
-#         for raw_text, offsets in training_data:
-#             doc = nlp.make_doc(raw_text)
-#             gold = GoldParse(doc, entities=offsets)
-#             nlp.tagger(doc)
-#             loss += nlp.entity.update(doc, gold)
-
-#         yield loss / len(training_data)
-
-#     nlp.end_training()
-
 def train_model(train_data, labels = ["redacted"], model=None, new_model_name='new_model', output_dir=None, n_iter=1):
     """Setting up the pipeline and entity recognizer, and training the new entity."""
     if model is not None:
@@ -132,6 +92,7 @@ def train_model(train_data, labels = ["redacted"], model=None, new_model_name='n
     else:
         nlp = spacy.blank('en')  # create blank Language class
         print("Created blank 'en' model")
+
     if 'ner' not in nlp.pipe_names:
         ner = nlp.create_pipe('ner')
         nlp.add_pipe(ner)
